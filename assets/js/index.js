8f58c4b74aaeef379c23a8f41bd8d3db39c92040
João Pedro <devcontatojoao@gmail.com>
2025-02-27 15:59:10 -0300

    2.0

--- assets/js/index.js
+++ assets/js/index.js
@@ -44,12 +44,19 @@ inputsForm.forEach(input => {
   });
 });
 
+const whatsappButton = document.querySelector("#whatsapp");
 const mobileMenuButton = document.querySelector(".toggle-menu");
 const mobileMenu = document.querySelector(".header-menu-mobile");
 mobileMenuButton.addEventListener("click", () => {
   mobileMenuButton.classList.toggle("active");
   mobileMenu.classList.toggle("active");
   document.body.classList.toggle("active");
+
+  if (mobileMenu.classList.contains("active")) {
+    whatsappButton.style.display = "none";
+  } else {
+    whatsappButton.style.display = "block";
+  }
 });
 
 const mobileMenuItems = document.querySelectorAll(
@@ -62,6 +69,57 @@ mobileMenuItems.forEach(menuItem => {
       mobileMenuButton.classList.remove("active");
       mobileMenu.classList.remove("active");
       document.body.classList.remove("active");
+    }
+  });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // Pega o alvo do link
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    // Calcula o deslocamento e faz o scroll com o offset
+    window.scrollTo({
+      top:
+        targetElement.offsetTop - document.querySelector("header").offsetHeight,
+      behavior: "smooth" // Para rolar suavemente
+    });
+  });
+});
+
+// const faqQuestions = document.querySelectorAll(".faq-question");
+
+// // Itera sobre os botões e adiciona um evento de clique
+// faqQuestions.forEach(question => {
+//   question.addEventListener("click", () => {
+//     const answer = question.nextElementSibling;
+
+//     // Alterna a classe "show" para aplicar o efeito suave
+//     if (answer.classList.contains("show")) {
+//       answer.classList.remove("show");
+//     } else {
+//       answer.classList.add("show");
+//     }
+//   });
+// });
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  const question = item.querySelector(".faq-question");
+  const answer = item.querySelector(".faq-answer");
+
+  question.addEventListener("click", () => {
+    item.classList.toggle("active"); // Alterna a classe 'active' ao clicar
+
+    // Verifica se o item está ativo para expandir ou contrair a resposta
+    if (item.classList.contains("active")) {
+      answer.style.maxHeight = answer.scrollHeight + "px"; // Define max-height conforme o conteúdo
+    } else {
+      answer.style.maxHeight = 0; // Recolhe a resposta
     }
   });
 });
